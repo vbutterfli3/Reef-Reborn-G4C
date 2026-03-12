2c85523735918fa4c64e2ef627f2efcf36647f65
Vanessa <nessag0118@gmail.com>
2026-03-11 23:05:17 -0400

    populate lvls 1 and 2, add fish dialogue

--- Assets/Reef Reborn/scripts/PlayerInventory.cs
+++ Assets/Reef Reborn/scripts/PlayerInventory.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
-    public int maxSlots = 7;
+    public int maxSlots = 12;
     public int totalCollected = 0;
     private int currentSlots = 0;
     public SpriteRenderer ESprite;
     public TextMeshProUGUI trashtext;
+    public GameObject ogcoral; 
     public GameObject coral1;
     public GameObject coral2;
     public GameObject coral3;
 
     public GameObject wall1;
     public GameObject wall2;
-    public GameObject wall3;
+    
 
 
 
@@ -44,20 +45,20 @@ public class PlayerInventory : MonoBehaviour
         totalCollected += currentSlots;
         currentSlots = 0;
         trashtext.text = currentSlots + "/" + maxSlots;
-        if (totalCollected > 3)
+        if (totalCollected >= 3)
         {
             coral1.SetActive(true);
             wall1.SetActive(false);
+            ogcoral.SetActive(false);
         }
-        if (totalCollected > 6)
+        if (totalCollected >= 6)
         {
             coral2.SetActive(true);
             wall2.SetActive(false);
         }
-        if (totalCollected > 9)
+        if (totalCollected >= 9)
         {
             coral3.SetActive(true);
-            wall3.SetActive(false);
         }
     }
 
