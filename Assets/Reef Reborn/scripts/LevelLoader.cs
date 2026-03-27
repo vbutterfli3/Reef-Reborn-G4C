ed1fc15d07388ccafff7e43de6d997b79b142e09
Vanessa <nessag0118@gmail.com>
2026-03-27 12:41:50 -0400

    random menu animated

---
+++ Assets/Reef Reborn/scripts/LevelLoader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public string nextScene;
+   // public Animator transition;
+   // public float transitionTime = 1;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey) ;
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+      //  if (Input.anyKey);
+      //  {
+          //  LoadNextLevel();
+      //  }
+        
+    }
+
+  //  public void LoadNextLevel()
+  //  {
+   //     StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+ //   }
+  //  IEnumerator LoadLevel(int levelIndex)
+   // {
+     //   transition.SetTrigger("Start");
+ //       yield return new WaitForSeconds(transitionTime);
+  //  }
+
+}
